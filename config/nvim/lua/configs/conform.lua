3e9c81f9417fa14d8a385874b49ea40be5a51a87
Gousev Ivan <vanyagousev@gmail.com>
2025-07-26 22:29:00 +0300

    clang-format added and new plugin

--- config/nvim/lua/configs/conform.lua
+++ config/nvim/lua/configs/conform.lua
@@ -1,15 +1,24 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+    formatters_by_ft = {
+        lua = { "stylua" },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        -- css = { "prettier" },
+        -- html = { "prettier" },
+    },
+    formatters = {
+        ["clang-format"] = {
+            command = "clang-format",
+            args = { "-assume-filename=%:p", "-style=file" },
+            stdin = true,
+        },
+    },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
 }
 
-return options
+require("conform").setup(options)
